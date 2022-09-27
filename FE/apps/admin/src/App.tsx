d531a73b2e054513459c4885e35afbaa8efbe3ba
Shenghang Zhu <67240191+zyhzsh@users.noreply.github.com>
2022-09-27 21:59:25 +0200

    init: init project for front end

---
+++ FE/apps/admin/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">Admin Application</div>;
+}
+
+export default App;
---
+++ FE/apps/voucker-web/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Web() {
+  return (
+    <div>
+      <h1>Voucker Platform</h1>
+    </div>
+  );
+}
