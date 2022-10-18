55ad9c3306034df2ff676c17c862b391f639be52
Shenghang Zhu <67240191+zyhzsh@users.noreply.github.com>
2022-10-18 18:26:14 +0200

    remove unused code

--- FE/apps/voucker-platform/pages/api/test.ts
+++ FE/apps/voucker-platform/pages/api/test.ts
@@ -1,11 +1,12 @@
 import { NextApiResponse, NextApiRequest } from 'next';
 import { getAccessToken } from '@auth0/nextjs-auth0';
-
+import { isVendor } from '../../utls/roleVaildator';
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { accessToken } = await getAccessToken(req, res);
-  console.log(accessToken);
+  // const result = isVendor(accessToken || '');
+
   return res.status(200).json(`acc token is:${accessToken}`);
 }
--- FE/apps/voucker-platform/pages/voucher/[voucherId].tsx
+++ FE/apps/voucker-platform/pages/voucher/[voucherId].tsx
@@ -4,14 +4,12 @@ import Navbar from '../../components/Common/Navbar';
 const VoucherDetail = (props: any) => {
   return (
     <>
-      s
       <Navbar />
     </>
   );
 };
 
 export default VoucherDetail;
-// This gets called on every request
 
 export async function getServerSideProps() {
   return { props: { test: 'sssfsds' } };
