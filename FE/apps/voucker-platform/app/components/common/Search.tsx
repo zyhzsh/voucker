db15a7da80c96ffb57fda1898790ae01ff260b60
Shenghang Zhu <67240191+zyhzsh@users.noreply.github.com>
2022-12-14 19:28:01 +0100

    inventory,location,category

--- FE/apps/voucker-platform/app/components/common/Search.tsx
+++ FE/apps/voucker-platform/app/components/common/Search.tsx
@@ -13,14 +13,14 @@ const Search: React.FC<SearchProps> = ({ onSearch }) => {
 
   return (
     <form className="flex flex-col items-center" onSubmit={onSearchHandler}>
-      <div className="flex w-full pl-20 pt-2">
+      <div className="pl-10 flex w-full md:pl-20 pt-2">
         <input
           type="text"
           className="flex-1 p-2 border-2 rounded-md focus:border-teal-700 mr-8"
         />
         <button
           type="submit"
-          className="flex w-60  mr-20 text-center justify-center font-mono  items-center rounded border border-transparent bg-teal-600 py-2 px-2 font-mon text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 "
+          className="hidden md:flex w-60  mr-20 text-center justify-center font-mono  items-center rounded border border-transparent bg-teal-600 py-2 px-2 font-mon text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 "
         >
           <MagnifyingGlassIcon className="w-4 mr-2" />
           Search
