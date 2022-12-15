0cb67219f2254e7a5bea78d541b6c30797b48119
Shenghang Zhu <67240191+zyhzsh@users.noreply.github.com>
2022-12-15 23:34:59 +0100

    test automation delolyment

--- BE/apps/category/src/category.controller.ts
+++ BE/apps/category/src/category.controller.ts
@@ -1,16 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { CategoryService } from './category.service';
 
-@Controller('')
+@Controller('api/category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Get()
-  healthCheck() {
-    return 'OK';
-  }
-
-  @Get('api/category')
   getCategory() {
     return this.categoryService.getCategory();
   }
